060d7bb9f1e46445ca8d1b3ed3c89af815d33104
MTBakalov18 <MTBakalov18@codingburgas.bg>
2022-12-14 15:45:09 +0200

    Almost finished

--- src/Guest.java
+++ src/Guest.java
@@ -1,2 +1,77 @@
-package PACKAGE_NAME;public class Guest {
+public class Guest {
+    String name;
+    String id;
+    int status;
+    int nights;
+    double bed;
+    String city;
+
+    public Guest(String name, String id, int status, int nights,double bed, String city) {
+        this.name = name;
+        this.id = id;
+        this.status = status;
+        this.nights = nights;
+        this.bed = bed;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+
+    public void setNights(int nights) {
+        this.nights = nights;
+    }
+
+    public double getBed() {
+        return bed;
+    }
+
+    public void setBed(double bed) {
+        this.bed = bed;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", status=" + status +
+                ", nights=" + nights +
+                ", bed=" + bed +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int option;
+        Scanner s = new Scanner(System.in);
+        ArrayList<Guest> Guest = new ArrayList<>();
+
+
+        while (true) {
+
+            System.out.println("Menu");
+            System.out.println("1. Insert data");
+            System.out.println("2. Print data");
+            System.out.println("3. Print names and paid price");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            option = s.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println();
+                    Guest.add(insertData(s));
+                    break;
+                case 2:
+                    System.out.println();
+                    printData(Guest);
+                    break;
+                case 3:
+                    System.out.println("Not done yet");
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Invalid option!");
+            }
+        }
     }
-}
+
+    public static void printData(ArrayList<Guest> Guest) {
+        for (Guest h : Guest) {
+            System.out.println(h.toString());
+        }
+    }
+
+    public static Guest insertData(Scanner s) {
+
+        String nothing = s.nextLine();
+        String name;
+        String id;
+        int status;
+        int nights;
+        double beds;
+        String city;
+
+
+        System.out.print("Name: ");
+        name = s.nextLine();
+        System.out.print("ID: ");
+        id = s.nextLine();
+        System.out.print("relationship status: 1 for engaged or 2 for single: ");
+        status = Integer.parseInt(s.nextLine());
+        System.out.print("Nights: ");
+        nights = Integer.parseInt(s.nextLine());
+        System.out.print("Price for beds:");
+        beds = Integer.parseInt(s.nextLine());
+        System.out.print("City: ");
+        city = s.nextLine();
+
+        return new Guest(name, id, status, nights, beds, city);
+    }
+
+    }
